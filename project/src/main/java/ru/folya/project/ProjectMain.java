0653711900acf0f6755d25522ed1ba8727ac4514
matabba <105707300+matabba@users.noreply.github.com>
2022-10-12 15:38:25 +0300

    Update ProjectMain.java

--- project/src/main/java/ru/folya/project/ProjectMain.java
+++ project/src/main/java/ru/folya/project/ProjectMain.java
@@ -1,53 +1,49 @@
-package ru.folya.project;
-   import  java.util.Scanner;
+import java.util.Scanner;
 public class practice {
-    static Scanner scanner = new Scanner (System.in);
-    public static void main (String [] args) {
-    int num1 = getInt();
-    int num2 = getInt();
-    char operation = getOperation ();
-    int result = calc (num1,num2,operation);
-    System.out.println ("Operation result: " +result)}
-}
-    public static int getInt() {
-    System.out.println("Enter number:");
-    int num;
-    if (scanner.hasNextInt ())){
-    num=scanner.nextInt();
-            }else{
-    System.out.println ("Error! incorect number")}
-            num = getInt();
-}
-            return num;
-}
 
-public static char getOperation (){
-    System.out.println("Enter operation");
-    char operation;
-    if (scanner.hasNext()) }
-        operation = getopertaion();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Type First Number");
+        int num1 = sc.nextInt();
+
+        System.out.println("Type Second Number");
+        int num2 = sc.nextInt();
+
+        System.out.println("Type Operation");
+        sc.nextLine();
+        String action = sc.nextLine();
+        action = scanner.nextline();
+
+        int result = 0;
+        System.out.println("Operation result: " + result);
+
+        int res;
+
+        switch (action) {
+            case "+":
+                res = num1 + num2;
+                System.out.println("Result " + result);
+                break;
+            case "-":
+                res = num1 - num2;
+                System.out.println("Result " + result);
+                break;
+            case "*":
+                res = num1 * num2;
+                System.out.println("Result " + result);
+                break;
+            case "/":
+                if(num == 0)
+                System.out.print("Error");
+            else
+                res = num1 / num2;
+                System.out.println("Result " + result);
+                break;
+            default:
+                System.out.println("Wrong.");
+
+
         }
-        return operation;
-        }
-public static int cacl(int num1, int num2, char operation) {
-     int result;
-     switch (operation){
-        case'+';
-        result+num1+num2;
-        break;
-        case'-';
-        result+num1-num2;
-        break;
-        case'*';
-        result+num1*num2;
-        break;
-        case'/';
-        break;
-default:
-        System.out.prinln("Operation not true. Enter number.");
-        result=cacl(num1,num2,getOperation()); //recursion
-        }
-        return result:
-        }
-        }
-        }
+    }
+}
